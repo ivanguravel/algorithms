5d1f10a723b922bcdeae6e90360130337115bb8a
ivan.zhuravel <ivanguravel@gmail.com>
2020-04-07 21:58:59 +0300

    dynamic programming https://acm.timus.ru/problem.aspx?space=1&num=1119

---
+++ src/main/java/org/ivzh/dynamic/programming/Metro.java
@@ -0,0 +1,137 @@
+package org.ivzh.dynamic.programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.math.BigDecimal;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+public class Metro {
+
+        private void solve() {
+
+            int n = nextInt();
+
+            int[] dp = new int[n+1];
+            dp[1]= 0;
+            dp[2]= 1;
+            dp[3]= 2;
+
+            for (int i = 4; i <= n; i++) {
+                int buffer = dp[i - 1] + 1;
+                if (i % 3==0) {
+                    buffer = min(buffer, dp[i / 3] + 1);
+                } else if (i % 2==0) {
+                    buffer = min(buffer, dp[i / 2] + 1);
+                }
+
+                dp[i] = buffer;
+            }
+
+            sout(dp[n]);
+
+        }
+
+        public static void main(String[] args) {
+            new Metro().run();
+        }
+
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+        private PrintWriter writer;
+
+        private void run() {
+            try {
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                tokenizer = null;
+                writer = new PrintWriter(System.out);
+                solve();
+                reader.close();
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        private int nextInt() {
+            return parseInt(nextToken());
+        }
+
+        private long nextLong() {
+            return parseLong(nextToken());
+        }
+
+        private double nextDouble() {
+            return parseDouble(nextToken());
+        }
+
+        private int[] nextArr(int size) {
+            return stream(new int[size]).map(c -> nextInt()).toArray();
+        }
+
+        private long[] nextArrL(int size) {
+            return stream(new long[size]).map(c -> nextLong()).toArray();
+        }
+
+        private double[] nextArrD(int size) {
+            return stream(new double[size]).map(c -> nextDouble()).toArray();
+        }
+
+        private char[][] nextCharMatrix(int n) {
+            return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
+        }
+
+        private int[][] nextIntMatrix(final int n, final int m) {
+            return range(0, n).mapToObj(i -> nextArr(m)).toArray(int[][]::new);
+        }
+
+        private double[][] nextDoubleMatrix(int n, int m) {
+            return range(0, n).mapToObj(i -> nextArr(m)).toArray(double[][]::new);
+        }
+
+        private String nextToken() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        private void souf(String format, Object... args) {
+            writer.printf(format, args);
+        }
+
+        private void sout(Object o) {
+            writer.print(o);
+        }
+
+        private void newLine() {
+            writer.println();
+        }
+
+        private void soutnl(Object o) {
+            sout(o);
+            newLine();
+        }
+
+        private int max(int a, int b) {
+            return Math.max(a, b);
+        }
+
+        private int min(int a, int b) {
+            return Math.min(a, b);
+        }
+
+
+}
