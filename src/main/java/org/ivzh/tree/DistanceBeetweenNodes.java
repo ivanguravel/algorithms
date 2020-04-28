813c6add5be206f9a6547774c59dc44884c3ab35
ivan.zhuravel <ivanguravel@gmail.com>
2020-04-28 22:24:28 +0300

    metro

---
+++ src/main/java/org/ivzh/tree/DistanceBeetweenNodes.java
@@ -0,0 +1,28 @@
+package org.ivzh.tree;
+
+import org.ivzh.graph.MetroInEkaterinburg;
+
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+// https://acm.timus.ru/problem.aspx?space=1&num=1471
+public class DistanceBeetweenNodes {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        new DistanceBeetweenNodes().solve(in, out);
+
+        out.flush();
+    }
+
+    void solve(Scanner in, PrintWriter out) {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int m = in.nextInt();
+
+
+    }
+
+}
