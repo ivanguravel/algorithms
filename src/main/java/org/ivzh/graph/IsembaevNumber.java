ec7c8427415d2f7859ea52ef63b3633fecbdb787
ivan.zhuravel <ivanguravel@gmail.com>
2020-04-30 21:01:28 +0300

    IsembaevNumber

---
+++ src/main/java/org/ivzh/graph/IsembaevNumber.java
@@ -0,0 +1,11 @@
+package org.ivzh.graph;
+
+import java.util.List;
+
+// https://acm.timus.ru/problem.aspx?space=1&num=1837
+public class IsembaevNumber {
+
+    public static void main(String[] args) {
+
+    }
+}
