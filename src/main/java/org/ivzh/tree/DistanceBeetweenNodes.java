3b44a5c2d934db18191673bde2c091bb49582675
Ivan Zhuravel <ivanguravel@gmail.com>
2020-05-16 23:19:05 +0300

    ts

--- src/main/java/org/ivzh/tree/DistanceBeetweenNodes.java
+++ src/main/java/org/ivzh/tree/DistanceBeetweenNodes.java
@@ -1,6 +1,5 @@
 package org.ivzh.tree;
 
-import org.ivzh.graph.MetroInEkaterinburg;
 
 import java.io.PrintWriter;
 import java.util.*;
@@ -8,61 +7,107 @@ import java.util.*;
 // https://acm.timus.ru/problem.aspx?space=1&num=1471
 public class DistanceBeetweenNodes {
 
-    List<GraphNode>[] graph;
-    int[][] pairs;
-    int[] distances;
-    int[] depth;
-    int root;
+    List<Integer> graph[];
+    int[] lev;
+    int memo[][], log;
+
+    List<Integer> result;
 
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-
         new DistanceBeetweenNodes().solve(in, out);
-
         out.flush();
     }
 
     public void solve(Scanner in, PrintWriter out) {
         int n = in.nextInt();
-        List<GraphNode>[] graph = new ArrayList[n];
-        for (int i = 0; i < n; ++i) {
+        graph = new ArrayList[n+1];
+        memo = new int[n + 1][20];
+
+        // black magic
+        log = (int)Math.ceil(Math.log(n) / Math.log(2));
+
+        for (int i = 0; i <= n; ++i) {
             graph[i] = new ArrayList<>();
         }
-        this.pairs = new int[n][17];
-        this.distances = new int[n];
-        this.depth = new int[n];
-        this.root = 0;
+
+        this.lev = new int[n + 1];
+
         for (int i = 1; i < n; ++i) {
             int u = in.nextInt();
             int v = in.nextInt();
             int w = in.nextInt();
-            graph[u].add(new GraphNode(v, w));
-            graph[v].add(new GraphNode(u, w));
+            graph[w].add(v);
+            graph[w].add(u);
+        }
+
+        dfs(1, 1);
+        int m = in.nextInt();
+        this.result = new ArrayList<>(m + 1);
+
+        for (int i =0 ; i < m; i++) {
+            int u = in.nextInt();
+            int v = in.nextInt();
+            result.add(distanceBetweenNodes(u, v));
+        }
+
+        for (Integer i : result) {
+            System.out.println(i);
         }
     }
 
-    void dfs(int vertex, int high, int level, int newDistance) {
-        distances[vertex] = newDistance;
-        depth[vertex] = level;
-        pairs[vertex][0] = vertex == root ? vertex : high;
-        for (GraphNode e : graph[vertex]) {
-            if (e.vertex != high) {
-                dfs(e.vertex, vertex, level + 1, newDistance + e.weight);
+    int distanceBetweenNodes(int u, int v) {
+        int distanceOne = lev[u];
+        int distanceTwo = lev[v];
+        return distanceOne + distanceTwo - 2 * lca(u, v);
+    }
+
+    int lca(int u, int v)
+    {
+        // if v deeper than u - change placement
+        if (lev[u] < lev[v]) {
+            int temp = u;
+            u = v;
+            v = temp;
+        }
+
+        // let's call it binary search
+        for (int i = log; i >= 0; i--) {
+            if ((lev[u] - (int)Math.pow(2, i)) >= lev[v])
+                u = memo[u][i];
+        }
+
+        if (u == v) {
+            return u;
+        }
+
+        //
+        for (int i = log; i >= 0; i--) {
+            if (memo[u][i] != memo[v][i]) {
+                u = memo[u][i];
+                v = memo[v][i];
+            }
+        }
+
+        return memo[u][0];
+    }
+
+
+    // Pre-processing to calculate values of memo[][]
+    void dfs(int u, int p) {
+        // Using recursion formula to calculate
+        // the values of memo[][]
+        memo[u][0] = p;
+        for (int i = 1; i <= log; i++)
+            memo[u][i] = memo[memo[u][i - 1]][i - 1];
+        for (int v : graph[u]) {
+            if (v != p) {
+                // Calculating the level of each node
+                lev[v] = lev[u] + 1;
+                dfs(v, u);
             }
         }
     }
-
-
-    class GraphNode {
-        int vertex;
-        int weight;
-
-        public GraphNode(int vertex, int weight) {
-            this.vertex = vertex;
-            this.weight = weight;
-        }
-    }
-
 }
