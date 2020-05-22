5d01aa1c9befee18b2092e533fb362f8011b9996
ivan.zhuravel <ivanguravel@gmail.com>
2020-05-22 19:23:14 +0300

    changes

--- src/main/java/org/ivzh/graph/Labyrinth.java
+++ src/main/java/org/ivzh/graph/Labyrinth.java
@@ -1,9 +1,58 @@
 package org.ivzh.graph;
 
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 // https://acm.timus.ru/problem.aspx?space=1&num=1033
 public class Labyrinth {
 
-    public static void main(String[] args) {
+    int n = 0;
+    char[][] map = new char[33][33];
+    boolean[][] visited = new boolean[33][33];
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        new Labyrinth().solve(in, out);
+
+        out.flush();
+    }
+
+    private void solve(Scanner in, PrintWriter out) {
+        readInput(in, out);
+
+        Queue<Character> q = new LinkedList<>();
+
+        for (int i =0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                q.add(map[i][j]);
+            }
+        }
+
+        int answer = 0;
+        while (!q.isEmpty()) {
+            Character poll = q.poll();
+            if (poll == '#') {
+                ++answer;
+            }
+        }
+
+        System.out.println(answer * 9);
+    }
+
+    private void readInput(Scanner in, PrintWriter out) {
+        this.n = in.nextInt();
+        in.nextLine();
+        for (int i =0; i < n; i++) {
+            String s = in.nextLine();
+            char[] ch = s.toCharArray();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = ch[j];
+            }
+        }
+        System.out.println(map);
     }
 }
