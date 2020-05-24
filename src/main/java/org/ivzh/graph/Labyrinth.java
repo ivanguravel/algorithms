05d8c5b37b062597b9795eb93cdcf1dc2766a2eb
Ivan Zhuravel <ivanguravel@gmail.com>
2020-05-24 21:17:58 +0300

    ts

--- src/main/java/org/ivzh/graph/Labyrinth.java
+++ src/main/java/org/ivzh/graph/Labyrinth.java
@@ -9,8 +9,10 @@ import java.util.Scanner;
 public class Labyrinth {
 
     int n = 0;
-    char[][] map = new char[33][33];
-    boolean[][] visited = new boolean[33][33];
+    char[][] map = new char[40][40];
+    boolean[][] visited = new boolean[40][40];
+    int tox[] = {1,0,-1,0};
+    int toy[] = {0,1,0,-1};
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -24,23 +26,40 @@ public class Labyrinth {
     private void solve(Scanner in, PrintWriter out) {
         readInput(in, out);
 
-        Queue<Character> q = new LinkedList<>();
+        // as per conditions
+        map[0][1] = map[1][0] = map[n][n+1] = map[n+1][n] = '!';
 
-        for (int i =0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                q.add(map[i][j]);
+        int result = 9*( bfs(2,2) + bfs(n, n));
+
+        System.out.println(result);
+    }
+
+    int bfs(int x,int y) {
+        if(visited[x][y]) return 0;
+
+        Queue<Integer> quex = new LinkedList<>();
+        Queue<Integer> quey = new LinkedList<>();
+        quex.add(x); quey.add(y);
+        visited[x][y] = true;
+        int ans = 0;
+        while(!quex.isEmpty())
+        {
+            Integer nowx = quex.poll();
+            Integer nowy = quey.poll();
+            for(int i = 0; i < 4; i ++)
+            {
+                int nextx = nowx + tox[i];
+                int nexty = nowy + toy[i];
+                if(!visited[nextx][nexty] && map[nextx][nexty]=='.')
+                {
+                    visited[nextx][nexty] = true;
+                    quex.add(nextx);
+                    quey.add(nexty);
+                }
+                else if(map[nextx][nexty] == '#') ans ++;
             }
         }
-
-        int answer = 0;
-        while (!q.isEmpty()) {
-            Character poll = q.poll();
-            if (poll == '#') {
-                ++answer;
-            }
-        }
-
-        System.out.println(answer * 9);
+        return ans;
     }
 
     private void readInput(Scanner in, PrintWriter out) {
@@ -53,6 +72,5 @@ public class Labyrinth {
                 map[i][j] = ch[j];
             }
         }
-        System.out.println(map);
     }
 }
