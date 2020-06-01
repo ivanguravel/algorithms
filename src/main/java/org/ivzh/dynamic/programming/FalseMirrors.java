28cd776216a70f18217738b01c3129aea27a4085
ivan.zhuravel <ivanguravel@gmail.com>
2020-06-01 20:08:50 +0300

    changes

--- src/main/java/org/ivzh/dynamic/programming/FalseMirrors.java
+++ src/main/java/org/ivzh/dynamic/programming/FalseMirrors.java
@@ -29,9 +29,13 @@ public class FalseMirrors {
         for(int shot = 0; shot < Math.pow(2, n); ++shot) {
             damage[shot] = 0;
 
-            for(int i = 0;i<n;++i)
-                if((shot & (1<<i))==0)
+            // calculate over damage different shots by masks
+            for(int i = 0;i<n;++i) {
+                //
+                if ((shot & (1 << i)) == 0) {
                     damage[shot] += balcons.get(i);
+                }
+            }
         }
         System.out.println(solve(0));
     }
@@ -44,20 +48,28 @@ public class FalseMirrors {
             for(int i = 0;i<n;++i){
                 boolean valid = false;
 
-                int shot2 = shot;
-                for(int j = 0;j < 3;++j) {
+                int shot4SmallestDamage = shot;
+                // going for every 3 elements
+                for(int j = 0; j < 3; ++j) {
+                    // find mask which is present in array
                     if ((shot & (1 << ((i + j) % n))) == 0) {
-                        shot2 = shot2 | (1 << ((i + j) % n));
+                        // find smallest element which is suitable for us
+                        shot4SmallestDamage = shot4SmallestDamage | (1 << ((i + j) % n));
                         valid = true;
                     }
                 }
 
-                if(!valid) continue;
+                if(!valid) {
+                    // if our masks are not valid - skip loop iteration
+                    continue;
+                }
 
-                int temp = solve(shot2) + damage[shot2];
+                // find smallest element 4 smallest damage + current smallest damage :)
+                int smallestDamage = solve(shot4SmallestDamage) + damage[shot4SmallestDamage];
 
-                if(result == Integer.MIN_VALUE || temp<result) {
-                    result = temp;
+                // if overall smallest damage > current which is found - use current smallest damage
+                if(result == Integer.MIN_VALUE || smallestDamage < result) {
+                    result = smallestDamage;
                 }
             }
 
@@ -75,8 +87,8 @@ public class FalseMirrors {
         balcons = new ArrayList<>(n+1);
         String[] split = in.nextLine().split(" ");
         balcons.addAll(Stream.of(split).map(Integer::valueOf).collect(Collectors.toList()));
-        cache = new int[n * 100];
-        damage = new int[n * 100];
+        cache = new int[120_000];
+        damage = new int[120_000];
         Arrays.fill(cache, Integer.MIN_VALUE);
     }
 }
