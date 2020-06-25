3b23f2c3680a004d7d27bd89b1df2017f1768abe
ivanguravel <ivanguravel@gmail.com>
2020-06-25 17:12:52 +0300

    changes

--- src/main/java/org/ivzh/lists/BitwiseLinkedList2Integer.java
+++ src/main/java/org/ivzh/lists/BitwiseLinkedList2Integer.java
@@ -5,4 +5,21 @@ public class BitwiseLinkedList2Integer {
     public static void main(String[] args) {
         getDecimalValue(head);
     }
+    
+    public int getDecimalValue(ListNode head) {
+        String result = "";
+        while (head != null) {
+            result = result + head.val;
+            head = head.next;
+        }
+        return Integer.parseInt(result, 2);
+    }
+    
+ public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
 }
