04229d239683150de86438b9a5f8667409cfd09a
ivanguravel <ivanguravel@gmail.com>
2020-07-02 16:01:25 +0300

    Update NonTrivialNumbers.java

--- src/main/java/org/ivzh/algebra/NonTrivialNumbers.java
+++ src/main/java/org/ivzh/algebra/NonTrivialNumbers.java
@@ -3,9 +3,11 @@ package org.ivzh.algebra;
 import java.io.PrintWriter;
 import java.util.*;
 
+
 public class NonTrivialNumbers {
 
     int n, m;
+    int maxPrime = Integer.MIN_VALUE;
     Set<Integer> primes;
 
     public static void main(String[] args) {
@@ -17,12 +19,33 @@ public class NonTrivialNumbers {
 
     void solve(Scanner in, PrintWriter out) {
         readData(in);
-        primesInRange(n, m);
+
+        if (n == m) {
+            out.println(n);
+            out.flush();
+            return;
+        }
+
+        if (n == 1) {
+            out.println(1);
+            out.flush();
+            return;
+        }
+
+        if (m > 10_000) {
+            primesInRange(m - 100, m);
+        } else {
+            primesInRange(n, m);
+        }
+
         double globalMinimum = Double.MAX_VALUE;
         int result = 0;
-        int previousPrime = 1;
+        int previousPrime = 0;
         for (int i = n; i <= m; i++) {
-            if (this.primes.isEmpty()) {
+            if (m > 10000) {
+                result = maxPrime;
+                break;
+            } else if (this.primes.isEmpty()) {
                 double localMinimum = (double) (calculateDividersSum(i) / i);
 
                 if (localMinimum <= globalMinimum) {
@@ -46,17 +69,18 @@ public class NonTrivialNumbers {
     double calculateDividersSum(int k) {
 
         double result = 1;
-        for (int i = 2; (1<<i) <= k; i++) {
+        for (int i = 2; (1 << i) <= k; i++) {
             if (k % i == 0) {
                 result = result + i;
-                if (i*i != k) {
-                    result = result + (double)(k / i);
+                if (i * i != k) {
+                    result = result + (double) (k / i);
                 }
 
             }
         }
         return result;
     }
+
 
     void primesInRange(int a, int b) {
         for (int i = a; i <= b; i++) {
@@ -73,6 +97,9 @@ public class NonTrivialNumbers {
             // print the number if prime
             if (isPrimeNumber) {
                 primes.add(i);
+                if (i > maxPrime) {
+                    maxPrime = i;
+                }
             }
         }
     }
