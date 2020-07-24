8fc74fc92f7600905b1a6c12b32bc9493841d392
Ivan Zhuravel <ivanguravel@gmail.com>
2020-07-24 23:16:35 +0300

    rotate arr

--- src/main/java/org/ivzh/arrays/RotateArray.java
+++ src/main/java/org/ivzh/arrays/RotateArray.java
@@ -8,18 +8,21 @@ public class RotateArray {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6,7};
         new RotateArray().rotate(nums, 3);
+        for (Integer i : nums) {
+            System.out.print(i + " ");
+        }
     }
 
     public void rotate(int[] nums, int k) {
         for (int i = 0; i < k; i++) {
             int temp = nums[nums.length -1];
-            Integer buffer = nums[i + 1];
-            Integer buffer1 = nums[i + 1];
-            nums[i+1] = nums[i];
+            int buffer = nums[i];
             for (int j = i + 1; j < nums.length; j++) {
-                buffer = nums[i + 1];
-                nums[j+1] = nums[j];
+                int t = nums[i + 1];
+                nums[i + 1] = buffer;
+                buffer = t;
             }
+            nums[i] = temp;
         }
     }
 }
