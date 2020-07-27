781305773d59f4f5baa4d5fee7611b9019f51bc6
Ivan Zhuravel <ivanguravel@gmail.com>
2020-07-27 14:02:39 +0300

    bs

--- src/main/java/org/ivzh/binary/search/KokoEatsBananas.java
+++ src/main/java/org/ivzh/binary/search/KokoEatsBananas.java
@@ -6,11 +6,12 @@ public class KokoEatsBananas {
 
 
     public static void main(String[] args) {
-
+        int[] arr = {30,11,23,4,20};
+        System.out.println(new KokoEatsBananas().minEatingSpeed(arr, 5));
     }
 
     public int minEatingSpeed(int[] piles, int H) {
-        int rightBound = Arrays.stream(piles).max().getAsInt();
+        int rightBound = 1_000_000_000;
         int leftBound = 1;
 
         while (leftBound < rightBound) {
@@ -24,8 +25,16 @@ public class KokoEatsBananas {
         return leftBound;
     }
 
-    private boolean canKokoEatsBanasFrom(int[] piles, int middle, int h) {
+    private boolean canKokoEatsBanasFrom(int[] piles, int middle, int H) {
+        int K = 0;
+        for (Integer i : piles) {
+            if (i % middle == 0) {
+                K = K + i / middle;
+            } else {
+                K = K + i / middle + 1;
+            }
+        }
 
-        return true;
+        return K <= H;
     }
 }
