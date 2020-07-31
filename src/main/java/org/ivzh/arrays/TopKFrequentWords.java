06c121f83484ab8c9341a87ffd2db18c1beba63f
Ivan Zhuravel <ivanguravel@gmail.com>
2020-07-31 21:47:18 +0300

    https://leetcode.com/problems/top-k-frequent-words/

--- src/main/java/org/ivzh/arrays/TopKFrequentWords.java
+++ src/main/java/org/ivzh/arrays/TopKFrequentWords.java
@@ -4,12 +4,15 @@ package org.ivzh.arrays;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
 // https://leetcode.com/problems/top-k-frequent-words/
 public class TopKFrequentWords {
 
 
     public static void main(String[] args) {
-        System.out.println(new TopKFrequentWords().topKFrequent(Arrays.asList("i", "love", "leetcode", "i", "love", "coding"), 4));
+        List<String> strings = Arrays.asList("i", "love", "leetcode", "i", "love", "coding");
+        String[] r = new String[strings.size()];
+        System.out.println(new TopKFrequentWords().topKFrequent3(strings.toArray(r), 3));
     }
 
 
@@ -36,6 +39,59 @@ public class TopKFrequentWords {
 
         for (int i = 0; i < k; i++) {
             mostFrequent.add(entryList.get(i).getKey());
+        }
+
+        return mostFrequent;
+    }
+
+    // will be fastest in distributed computing
+    public List<String> topKFrequent2(String[] words, int k) {
+        return Arrays
+                .asList(words)
+                .parallelStream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toMap(w -> w, w -> 1, Integer::sum))
+                .entrySet()
+                .parallelStream()
+                .sorted(Collections.reverseOrder((o1, o2) -> {
+                    int compare = Long.compare(o1.getValue(), o2.getValue());
+                    return compare != 0 ? compare : o2.getKey().compareTo(o1.getKey());
+                }))
+                .collect(Collectors.toList())
+                .parallelStream()
+                .limit(k)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    // fastest - 94%
+    public List<String> topKFrequent3(String[] words, int k) {
+
+        Map<String, Long> map = new HashMap<>();
+
+        Long value;
+        for (String word : words) {
+            value = map.get(word);
+            if (value != null) {
+                map.put(word, value + 1);
+            } else {
+                map.put(word, 1L);
+            }
+        }
+
+        PriorityQueue<AbstractMap.SimpleEntry<String, Long>> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            int compare = Long.compare(o2.getValue(), o1.getValue());
+            return compare != 0 ? compare : o1.getKey().compareTo(o2.getKey());
+        });
+
+        for (Map.Entry<String, Long> e : map.entrySet()) {
+            priorityQueue.add(new AbstractMap.SimpleEntry<>(e.getKey(), e.getValue()));
+        }
+
+        List<String> mostFrequent = new LinkedList<>();
+        int index = 0;
+        while (!priorityQueue.isEmpty() && index++ < k) {
+            mostFrequent.add(priorityQueue.poll().getKey());
         }
 
         return mostFrequent;
