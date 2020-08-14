6891d598e62bb4f7449420b22bfbcd79b20d8e58
ivanguravel <ivanguravel@gmail.com>
2020-08-14 18:42:53 +0300

    Update MinStack.java

--- src/main/java/org/ivzh/lists/MinStack.java
+++ src/main/java/org/ivzh/lists/MinStack.java
@@ -1,57 +1,38 @@
 package org.ivzh.lists;
 
 import java.util.LinkedList;
-import java.util.List;
 
 
 // TODO
 // https://leetcode.com/problems/min-stack/
-public class MinStack {
-
-    List<Integer> list = new LinkedList();
+class MinStack {
+    
+    LinkedList<Integer> list = new LinkedList<>();
     Integer minimum = Integer.MAX_VALUE;
-    Integer top;
-
-    /** initialize your data structure here. */
+    
+     /** initialize your data structure here. */
     public MinStack() {
 
     }
-
-    public static void main(String[] args) {
-        MinStack minStack = new MinStack();
-
-        minStack.push(2);
-        minStack.push(0);
-        minStack.push(3);
-        minStack.push(0);
-
-        System.out.println(minStack.getMin());
-        minStack.pop();
-
-        System.out.println(minStack.getMin());
-        minStack.pop();
-
-        System.out.println(minStack.getMin());
-        minStack.pop();
-    }
-
+    
     public void push(int x) {
-        if (x < minimum) {
+        if (x <= minimum) {
             minimum = x;
         }
         list.add(x);
-        top = x;
     }
 
     public void pop() {
-        list.remove(top);
-        minimum = Integer.MAX_VALUE;
-        top = list.get(list.size() -1);
-        setMin();
+        Integer last = list.getLast();
+        list.removeLast();
+        if (minimum.equals(last)) {
+            minimum = Integer.MAX_VALUE;
+            setMin();
+        }
     }
 
     public int top() {
-        return top;
+        return list.getLast();
     }
 
     public int getMin() {
