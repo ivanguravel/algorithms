244b64383e00396d7889004037911845907893a3
ivanguravel <ivanguravel@gmail.com>
2020-08-17 16:25:27 +0300

    Create UnrealStoryFenvick.java

---
+++ src/main/java/org/ivzh/other/UnrealStoryFenvick.java
@@ -0,0 +1,107 @@
+package org.ivzh.other;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+// https://acm.timus.ru/problem.aspx?space=1&num=1491
+public class UnrealStoryFenvick {
+
+
+    int n;
+    int[][] data;
+    int[] result;
+
+    BinaryIndexedTree bit;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        new UnrealStory().solve(in, out);
+    }
+
+    void solve(Scanner in, PrintWriter out) {
+        readData(in);
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            builder.append(this.bit.getSum(i) + " ");
+        }
+
+        out.println(builder.toString());
+        out.flush();
+    }
+
+
+    // O(n^2)
+    void solve2(Scanner in, PrintWriter out) {
+        readData(in);
+
+        for (int[] d : data) {
+            for (int j = d[0]; j <= d[1]; j++) {
+                result[j] = result[j] + d[2];
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < n+1; i++) {
+            builder.append(result[i] + " ");
+        }
+
+        out.println(builder.toString());
+        out.flush();
+    }
+
+    private void readData(Scanner in) {
+        this.n = in.nextInt();
+        this.result = new int[n+1];
+
+        in.nextLine();
+
+        this.bit = new BinaryIndexedTree();
+        this.bit.constructBITree(n);
+
+        for (int i = 0; i < n+1; i++) {
+            String[] line = in.nextLine().split(" ");
+            Integer key1 = Integer.parseInt(line[0]);
+            Integer key2 = Integer.parseInt(line[1]);
+            Integer val = Integer.parseInt(line[2]);
+
+            this.bit.updateBIT(key2, val);
+            this.bit.updateBIT(key1 -1, -val);
+        }
+    }
+
+
+    static class BinaryIndexedTree {
+        // Max tree size
+        final static int MAX = 100000;
+
+        int BITree[] = new int[MAX];
+        int queryRange = 0;
+
+        int getSum(int index) {
+            int sum = 0;
+
+            for (int i = index; i <= queryRange; i += i & (-i)) {
+                sum = sum + BITree[i];
+            }
+
+            return sum;
+        }
+
+
+        public void updateBIT(int index, int val) {
+            for (int i = index; i > 0; i -= i & (-i)) {
+                BITree[i] = BITree[i] + val;
+            }
+        }
+
+        void constructBITree(int n) {
+            for (int i = 0; i <= n; i++) {
+                BITree[i] = 0;
+            }
+            this.queryRange = n;
+        }
+    }
+}
