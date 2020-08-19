20e7dd42b51ce4d22862a471bb7f245ccccea68b
ivanguravel <ivanguravel@gmail.com>
2020-08-19 17:53:15 +0300

    Update UnrealStory.java

--- src/main/java/org/ivzh/other/UnrealStory.java
+++ src/main/java/org/ivzh/other/UnrealStory.java
@@ -12,19 +12,16 @@ public class UnrealStory {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        new UnrealStory().scanline(in, out);
+        new UnrealStory().scanLine(in, out);
     }
 
-    void scanline(Scanner in, PrintWriter out) {
+    void scanLine(Scanner in, PrintWriter out) {
         readData(in);
-
-        for (int i = 1; i <= n; i++) {
-            result[i] = result[i] + result[i - 1];
-        }
 
         StringBuilder builder = new StringBuilder();
         for (int i = 1; i <= n; i++) {
-            builder.append(this.result[i] + " ");
+            result[i] = result[i] + result[i - 1];
+            builder.append(String.format("%s ", this.result[i]));
         }
 
         out.println(builder.toString());
@@ -37,11 +34,15 @@ public class UnrealStory {
 
         in.nextLine();
 
+        String[] line;
+        Integer key1;
+        Integer key2;
+        Integer val;
         for (int i = 0; i < n+1; i++) {
-            String[] line = in.nextLine().split(" ");
-            Integer key1 = Integer.parseInt(line[0]);
-            Integer key2 = Integer.parseInt(line[1]);
-            Integer val = Integer.parseInt(line[2]);
+            line = in.nextLine().split(" ");
+            key1 = Integer.parseInt(line[0]);
+            key2 = Integer.parseInt(line[1]);
+            val = Integer.parseInt(line[2]);
 
 
             result[key1] = result[key1] + val;
