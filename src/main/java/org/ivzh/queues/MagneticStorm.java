f3c9ac2513040773d6ee06d9129ff44fafc60f55
Ivan Zhuravel <ivanguravel@gmail.com>
2020-08-22 20:00:16 +0300

    fix

---
+++ src/main/java/org/ivzh/queues/MagneticStorm.java
@@ -0,0 +1,46 @@
+package org.ivzh.queues;
+
+
+import java.io.PrintWriter;
+import java.util.*;
+
+public class MagneticStorm {
+
+    int m;
+    List<Integer> values;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        new MagneticStorm().solve(in, out);
+    }
+
+    private void solve(Scanner in, PrintWriter out) {
+        readData(in);
+        Integer index = 0;
+        Integer max = Integer.MIN_VALUE;
+        for (Integer v : values) {
+            ++index;
+            max = Math.max(max, v);
+            if (index == this.m) {
+                out.println(max);
+                out.flush();
+                max = Integer.MIN_VALUE;
+                index = 0;
+            }
+        }
+    }
+
+    private void readData(Scanner in) {
+        this.m = in.nextInt();
+        this.values = new LinkedList<>();
+        while(true) {
+            int val = in.nextInt();
+            if (val != -1) {
+                this.values.add(val);
+            } else {
+                break;
+            }
+        }
+    }
+}
