b774515e3b4ef2a33748fa931ff954d92f904be0
Ivan Zhuravel <ivanguravel@gmail.com>
2020-09-05 00:01:50 +0300

    fix

--- src/main/java/org/ivzh/graph/OlegAndLittlePonies3.java
+++ src/main/java/org/ivzh/graph/OlegAndLittlePonies3.java
@@ -39,8 +39,8 @@ public class OlegAndLittlePonies3 {
 
         StringBuilder result = new StringBuilder(EMPTY);
 
-        for (boolean i : visited) {
-            result.append(i ? ONE : ZERO);
+        for (int i = 0; i < n; i++) {
+            result.append(visited[i] ? ONE : ZERO);
         }
 
         out.println(result.toString());
@@ -49,14 +49,15 @@ public class OlegAndLittlePonies3 {
 
     void dfs() {
         while (!queue.isEmpty()) {
-            List<Integer> stored = store.get(queue.pollFirst());
+            List<Integer> stored = store.get(queue.poll());
             if (stored != null) {
                 for (Integer integer : stored) {
-                    --counter[integer];
+
                     if (counter[integer] != 0) {
+                        --counter[integer];
                         List<Integer> integers = wishes.get(integer);
                         for (int i = 0; i < wishes.size(); i++) {
-                            if (integers != null && integers.size() >= i) {
+                            if (integers != null && integers.size() > i) {
                                 if (!visited[integers.get(i)]) {
                                     visited[integers.get(i)] = true;
                                     queue.push(integers.get(i));
@@ -80,6 +81,8 @@ public class OlegAndLittlePonies3 {
         this.counter = new int[4010];
         this.visited = new boolean[4010];
 
+        Arrays.fill(this.visited, false);
+
         in.nextLine();
 
 
@@ -88,7 +91,7 @@ public class OlegAndLittlePonies3 {
             String[] split = s.split(" ");
 
             String forToysRecognition = split[0];
-            String forWishes = split[0];
+            String forWishes = split[1];
 
             for (int j = 0; j < n; ++j) {
                 if (forToysRecognition.charAt(j) == ONE) {
