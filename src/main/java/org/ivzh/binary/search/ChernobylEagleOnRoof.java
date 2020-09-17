8ef7cfb269a66f82dc719f9679a3656cbd5a1546
ivanguravel <ivanguravel@gmail.com>
2020-09-17 20:42:31 +0300

    Update ChernobylEagleOnRoof.java

--- src/main/java/org/ivzh/binary/search/ChernobylEagleOnRoof.java
+++ src/main/java/org/ivzh/binary/search/ChernobylEagleOnRoof.java
@@ -17,25 +17,45 @@ public class ChernobylEagleOnRoof {
     }
 
     private void solve(Scanner in, PrintWriter out) {
-        int eggsCount = 1;
-        int floorsCount = 1;
 
-        for (int i = 2; i <= 1000; i++) {
+        for (int i = 0; i <= 1001; i++) {
+            // for dynamic work
+            dp[0][i] = Integer.MAX_VALUE;
+            if (i >= 1) {
+                // we can break 1 egg for each floors
+                dp[1][i] = i;
+                // we got each egg on 1 floor
+                dp[i][1] = 1;
+                //dp[i][0] = 0;
+            }
+        }
+
+        solve();
+        result(in, out);
+    }
+
+    private void solve() {
+        for (int i = 2; i <= 1001; i++) {
             binarySearch[i] = binarySearch[i / 2] + 1;
         }
 
         dp[1][1] = 1;
 
-        for (int i = 2; i <= 1000; i++) {
+        int eggsCount = 1001;
+        int floorsCount = 1001;
 
-            int min1 = dp[eggsCount - 1][i - 1];
-            int min2 = dp[eggsCount][floorsCount - i];
-
-            dp[eggsCount][floorsCount] = (int) Math.min(min1, min2);
+        for (int i = 2; i <= eggsCount; i++) {
+            for (int j = 2; j <= floorsCount; j++) {
+                Integer min = Integer.MAX_VALUE;
+                for (int floor = 1; floor <= j; floor++) {
+                    int min1 = dp[i - 1][floor - 1];
+                    int min2 = dp[i][j - floor];
+                    int current = (int) Math.max(min1, min2);
+                    min = (int) Math.min(current, min);
+                }
+                dp[eggsCount][floorsCount] = min +1;
+            }
         }
-
-
-        result(in, out);
     }
 
     private void result(Scanner in, PrintWriter out) {
@@ -45,8 +65,11 @@ public class ChernobylEagleOnRoof {
             if (eggs == 0 && floors == 0) {
                break;
             } else {
+
+
                 int answer = binarySearch[floors] <= eggs ? binarySearch[floors] : dp[eggs][floors];
-                out.print(answer);
+                out.println(answer);
+                out.flush();
             }
         }
     }
