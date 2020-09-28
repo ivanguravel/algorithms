5cf123284a1165dee3090f623e87cc67d6f289f5
ivanguravel <ivanguravel@gmail.com>
2020-09-28 20:27:50 +0300

    Create ZigZagTraversal.java

---
+++ src/main/java/org/ivzh/tree/ZigZagTraversal.java
@@ -0,0 +1,73 @@
+
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal
+class ZigZagTraversal {
+    
+   
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        zigzag(root, result);
+        //Collections.reverse(result);
+        return result;
+    }
+    
+    
+    private void zigzag(TreeNode root, List<List<Integer>> result) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Integer> forAdd;
+        if (root != null) {
+            q.add(root);
+            forAdd = new LinkedList<>();
+            forAdd.add(root.val);
+            result.add(forAdd);    
+        }
+        Integer level = 2;
+        TreeNode l;
+        
+        while (!q.isEmpty()) {
+            
+            l = q.poll();
+               
+            TreeNode left = l.left;
+            TreeNode right = l.right;
+            forAdd = new LinkedList<>();
+            
+            Integer leftVal = null;
+            Integer rightVal = null;
+            
+            if (right !=null) {
+                rightVal =  right.val;  
+                
+                
+                q.add(right);
+            }
+            
+            if (left !=null) {
+                leftVal =  left.val;   
+                
+                q.add(left);
+            }
+          
+            
+            if (level % 2 == 0) {
+                if (rightVal != null) {
+                   forAdd.add(rightVal);
+                }
+                if (leftVal != null) {
+                   forAdd.add(leftVal);
+                }
+            } else {
+                if (leftVal != null) {
+                   forAdd.add(leftVal);
+                }
+                if (rightVal != null) {
+                   forAdd.add(rightVal);
+                }
+            }
+            level = level +1;
+            if (!forAdd.isEmpty()) {
+               result.add(forAdd);
+            }
+        }
+        
+    }
+}
