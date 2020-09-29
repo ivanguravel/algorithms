2f23bd1eb910c32d23fecf15e90e81efb1843835
ivanguravel <ivanguravel@gmail.com>
2020-09-29 10:45:57 +0300

    Update ZigZagTraversal.java

--- src/main/java/org/ivzh/tree/ZigZagTraversal.java
+++ src/main/java/org/ivzh/tree/ZigZagTraversal.java
@@ -1,16 +1,19 @@
+package org.ivzh.tree;
 
-// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal
-class ZigZagTraversal {
-    
-   
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ZigZagTraversal {
+
+
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new LinkedList<>();
         zigzag(root, result);
-        //Collections.reverse(result);
         return result;
     }
-    
-    
+
+
     private void zigzag(TreeNode root, List<List<Integer>> result) {
         Queue<TreeNode> q = new LinkedList<>();
         List<Integer> forAdd;
@@ -18,56 +21,70 @@ class ZigZagTraversal {
             q.add(root);
             forAdd = new LinkedList<>();
             forAdd.add(root.val);
-            result.add(forAdd);    
+            result.add(forAdd);
         }
         Integer level = 2;
         TreeNode l;
-        
+
         while (!q.isEmpty()) {
-            
+
             l = q.poll();
-               
+
             TreeNode left = l.left;
             TreeNode right = l.right;
             forAdd = new LinkedList<>();
-            
+
             Integer leftVal = null;
             Integer rightVal = null;
-            
+
             if (right !=null) {
-                rightVal =  right.val;  
-                
-                
+                rightVal =  right.val;
+
+
                 q.add(right);
             }
-            
+
             if (left !=null) {
-                leftVal =  left.val;   
-                
+                leftVal =  left.val;
+
                 q.add(left);
             }
-          
-            
+
+
             if (level % 2 == 0) {
                 if (rightVal != null) {
-                   forAdd.add(rightVal);
+                    forAdd.add(rightVal);
                 }
                 if (leftVal != null) {
-                   forAdd.add(leftVal);
+                    forAdd.add(leftVal);
                 }
             } else {
                 if (leftVal != null) {
-                   forAdd.add(leftVal);
+                    forAdd.add(leftVal);
                 }
                 if (rightVal != null) {
-                   forAdd.add(rightVal);
+                    forAdd.add(rightVal);
                 }
             }
             level = level +1;
             if (!forAdd.isEmpty()) {
-               result.add(forAdd);
+                result.add(forAdd);
             }
         }
-        
+
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
