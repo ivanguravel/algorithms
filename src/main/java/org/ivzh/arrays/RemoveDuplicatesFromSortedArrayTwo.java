76cacabaa3dc55b9c70d85dc3004a9bc3b20ec54
ivanguravel <ivanguravel@gmail.com>
2020-10-20 10:49:26 +0300

    Update RemoveDuplicatesFromSortedArrayTwo.java

--- src/main/java/org/ivzh/arrays/RemoveDuplicatesFromSortedArrayTwo.java
+++ src/main/java/org/ivzh/arrays/RemoveDuplicatesFromSortedArrayTwo.java
@@ -5,14 +5,14 @@ import java.util.*;
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 class RemoveDuplicatesFromSortedArrayTwo {
     public int removeDuplicates(int[] nums) {
-        int k = 0;
+        int duplicatesCount = 0;
         Map<Integer, Integer> map = new HashMap<>();
         for(int j : nums){
             if(map.getOrDefault(j, 0) < 2){
-                nums[k++] = j;
+                nums[duplicatesCount++] = j;
             }
             map.put(j, map.getOrDefault(j, 0) + 1);
         }
-        return k;
+        return duplicatesCount;
     }
 }
