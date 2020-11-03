f84379ed4a7cb706e8c032b1c8b298ce6e548bea
Ivan Zhuravel <ivanguravel@gmail.com>
2020-11-03 23:43:51 +0200

    https://codeforces.com/contest/165/problem/C

---
+++ src/main/java/org/ivzh/greddy/algos/AnotherStringProblem.java
@@ -0,0 +1,21 @@
+package org.ivzh.greddy.algos;
+
+
+import java.util.Scanner;
+
+// https://codeforces.com/contest/165/problem/C
+public class AnotherStringProblem {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int k = in.nextInt();
+        String s = in.next();
+
+        int i =0;
+        int j = 0;
+        while (i < s.length()) {
+
+        }
+    }
+}
