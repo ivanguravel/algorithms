c167e50af3202e02b3ea85414c565518c64ba8d8
ivanguravel <ivanguravel@gmail.com>
2020-11-11 14:18:43 +0200

    Update AddTwoNumbers.java

--- src/main/java/org/ivzh/lists/AddTwoNumbers.java
+++ src/main/java/org/ivzh/lists/AddTwoNumbers.java
@@ -2,75 +2,77 @@ package org.ivzh.lists;
 
 import java.util.*;
 
- // https://leetcode.com/problems/add-two-numbers/
+// https://leetcode.com/problems/add-two-numbers/
 class AddTwoNumbers {
-   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int sum=l1.val+l2.val;
-        int temp=0;
-        if(sum>9){
-            sum=sum-10;
-            temp=1;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = l1.val + l2.val;
+        int temp = 0;
+        if (sum > 9) {
+            sum = sum - 10;
+            temp = 1;
         }
-        ListNode head=new ListNode();
-        ListNode l3=new ListNode(sum);
-        head=l3;
-        l1=l1.next;l2=l2.next;
-        while(l1!=null && l2!=null){ 
-            sum=l1.val+l2.val+temp;
-            if(sum>9){
-                sum=sum-10;
-                temp=1;
-                 ListNode tempLink=new ListNode(sum);
-                l3.next=tempLink;
-                l3=l3.next;
-            }else{
-                 ListNode tempLink=new ListNode(sum);
-                temp=0;
-                l3.next=tempLink;
-                l3=l3.next;
+        ListNode head = new ListNode();
+        ListNode l3 = new ListNode(sum);
+        head = l3;
+        l1 = l1.next;
+        l2 = l2.next;
+        while (l1 != null && l2 != null) {
+            sum = l1.val + l2.val + temp;
+            if (sum > 9) {
+                sum = sum - 10;
+                temp = 1;
+                ListNode tempLink = new ListNode(sum);
+                l3.next = tempLink;
+                l3 = l3.next;
+            } else {
+                ListNode tempLink = new ListNode(sum);
+                temp = 0;
+                l3.next = tempLink;
+                l3 = l3.next;
             }
-            l1=l1.next;l2=l2.next;
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        if(l1!=null){
-            while(l1!=null){
-                sum=l1.val+temp;
-                 if(sum>9){
-                sum=sum-10;
-                temp=1;
-                 ListNode tempLink=new ListNode(sum);
-                l3.next=tempLink;
-                l3=l3.next;
-            }else{
-                ListNode tempLink=new ListNode(sum);
-                temp=0;
-                l3.next=tempLink;
-                l3=l3.next;
+        if (l1 != null) {
+            while (l1 != null) {
+                sum = l1.val + temp;
+                if (sum > 9) {
+                    sum = sum - 10;
+                    temp = 1;
+                    ListNode tempLink = new ListNode(sum);
+                    l3.next = tempLink;
+                    l3 = l3.next;
+                } else {
+                    ListNode tempLink = new ListNode(sum);
+                    temp = 0;
+                    l3.next = tempLink;
+                    l3 = l3.next;
+                }
+                l1 = l1.next;
             }
-                l1=l1.next;
-            }
-        }else{
-              while(l2!=null){
-                sum=l2.val+temp;
-                      if(sum>9){
-                sum=sum-10;
-                temp=1;
-                 ListNode tempLink=new ListNode(sum);
-                l3.next=tempLink;
-                l3=l3.next;
-            }else{
-                 ListNode tempLink=new ListNode(sum);
-                temp=0;
-                l3.next=tempLink;
-                l3=l3.next;
-            }
-                  l2=l2.next;
+        } else {
+            while (l2 != null) {
+                sum = l2.val + temp;
+                if (sum > 9) {
+                    sum = sum - 10;
+                    temp = 1;
+                    ListNode tempLink = new ListNode(sum);
+                    l3.next = tempLink;
+                    l3 = l3.next;
+                } else {
+                    ListNode tempLink = new ListNode(sum);
+                    temp = 0;
+                    l3.next = tempLink;
+                    l3 = l3.next;
+                }
+                l2 = l2.next;
             }
         }
-        if(temp==1){
-            ListNode tempLink=new ListNode(1);
-            l3.next=tempLink;
-            l3=l3.next;
-                                       
+        if (temp == 1) {
+            ListNode tempLink = new ListNode(1);
+            l3.next = tempLink;
+            l3 = l3.next;
+
         }
         return head;
     }
