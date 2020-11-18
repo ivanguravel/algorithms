652be3609e8a5c894b7b0b1f8c1a9ac6d270d4ba
ivanguravel <ivanguravel@gmail.com>
2020-11-18 15:16:39 +0200

    Create Tribonacci.java

---
+++ src/main/java/org/ivzh/dynamic/programming/Tribonacci.java
@@ -0,0 +1,28 @@
+package org.izvh.dynamic.programming;
+
+// https://leetcode.com/problems/n-th-tribonacci-number/
+class Tribonacci {
+    public int tribonacci(int n) {
+        
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 1;
+        } else {
+        
+            int[] dp = new int[n+2];
+
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            }
+
+            return dp[n];
+        }
+    }
+}
