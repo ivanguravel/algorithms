4f6b5b58f49075b7c846d92cbc3274562fa06d16
Ivan Zhuravel <ivanguravel@gmail.com>
2021-01-02 23:14:40 +0200

    ac

--- src/main/java/org/ivzh/graph/Routing.java
+++ src/main/java/org/ivzh/graph/Routing.java
@@ -3,6 +3,7 @@ package org.ivzh.graph;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.lang.Integer.parseInt;
 
@@ -11,7 +12,12 @@ public class Routing {
 
 
     int n;
-    private Map<Integer, List<HostDescription>> graph;
+    private Map<Integer, Set<Integer>> graph;
+    private Map<Long, List<HostDescription>> subnets;
+    private Map<Integer, Set<Long>> ip2subnet;
+    int[] distance;
+    int[] parentNodes;
+    boolean[] visited;
     int start, end;
 
     private BufferedReader reader;
@@ -20,6 +26,7 @@ public class Routing {
 
     public static void main(String[] args) {
         new Routing().run();
+
     }
 
     private void run() {
@@ -37,45 +44,80 @@ public class Routing {
 
     private void solve() {
         readData();
+        provisionGraph();
         bfs();
         printer();
-
+        flush();
     }
 
     private void bfs() {
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
-        boolean[] visited = new boolean[n+2];
         visited[start] = true;
-        int[] d = new int[n+2];
+        parentNodes[start] = -1;
+
         Integer parent;
         while (!q.isEmpty()) {
             parent = q.poll();
-            for (Integer child : graph.get(parent))
+            for (Integer child : graph.get(parent)) {
+                if (!visited[child]) {
+                    distance[child] = distance[parent] + 1;
+                    parentNodes[child] = parent;
+                    visited[child] = true;
+                    q.add(child);
+                }
+            }
         }
 
     }
 
     private void printer() {
+        if (!visited[end]) {
+            println("No");
+        } else {
+            println("Yes");
+            Stack<Integer> stack = new Stack<>();
+            for (int i = end; i >= start; i = parentNodes[i]) {
+                stack.push(i);
+            }
 
+            while (!stack.empty()) {
+                print(String.format("%d ", stack.pop()));
+            }
+        }
     }
 
     private void readData() {
         n = nextInt();
         graph = new HashMap<>();
+        subnets = new HashMap<>();
+        ip2subnet = new HashMap<>();
+        distance =  new int[n+2];
+        parentNodes =  new int[n+2];
+        visited = new boolean[n+2];
 
         for (int i = 1; i <= n; i++) {
            int k = nextInt();
            int count = 0;
-
+           HostDescription hostDescription;
            while (count < k) {
-               List<HostDescription> pcHosts = graph.get(i);
+               hostDescription = new HostDescription(i, nextToken(), nextToken());
+               List<HostDescription> pcHosts = subnets.get(hostDescription.subnet);
                if (pcHosts == null) {
                    pcHosts = new LinkedList<>();
                }
-               pcHosts.add(new HostDescription(nextToken(), nextToken()));
-               graph.put(i, pcHosts);
+               pcHosts.add(hostDescription);
+               subnets.put(hostDescription.subnet, pcHosts);
                ++count;
+
+               //
+               Set<Long> subnet = ip2subnet.get(i);
+               if (subnet == null) {
+                   subnet = new HashSet<>();
+               }
+
+               subnet.add(hostDescription.subnet);
+               ip2subnet.put(i, subnet);
            }
         }
 
@@ -83,10 +125,40 @@ public class Routing {
         end = nextInt();
     }
 
+    private void provisionGraph() {
+        for (Integer node : ip2subnet.keySet()) {
+            Set<Long> subnetsHolder = ip2subnet.get(node);
+            if (subnetsHolder != null) {
+                for (Long subnet : subnetsHolder) {
+                    List<HostDescription> hostDescriptions = subnets.get(subnet);
+                    if (hostDescriptions != null) {
+                        List<Integer> integers = hostDescriptions.stream().map(hd -> hd.host).collect(Collectors.toList());
+                        Set<Integer> set = graph.get(node);
+                        if (set == null) {
+                            set = new HashSet<>();
+                        }
+                        set.addAll(integers);
+                        graph.put(node, set);
+                        for (Integer i : integers) {
+                            Set<Integer> set1 = graph.get(i);
+                            if (set1 == null) {
+                                set1 = new HashSet<>();
+                            }
+                            set1.add(node);
+                            graph.put(i, set1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     static class HostDescription {
+        int host;
         long subnet;
 
-        public HostDescription(String ip, String mask) {
+        public HostDescription(int host, String ip, String mask) {
+            this.host = host;
             long subnetConverted = toBits(ip);
             long maskConverted = toBits(mask);
             this.subnet = subnetConverted & maskConverted;
