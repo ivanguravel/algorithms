e393ecf1edde6a997522c2f0633861e64730d516
ivanguravel <ivanguravel@gmail.com>
2021-01-04 20:05:11 +0200

    Create Customs.java

---
+++ src/main/java/org/ivzh/sort/Customs.java
@@ -0,0 +1,99 @@
+package org.ivzh.sort;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+
+public class Customs {
+
+    int n;
+    int k;
+    long mxw;
+    long mxp;
+    long a;
+    int b;
+    Map<Long, Long> holder;
+
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+    private PrintWriter writer;
+
+    public static void main(String[] args) {
+        new Customs().run();
+    }
+
+    private void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out))) {
+            this.reader = reader;
+            this.writer = writer;
+            this.tokenizer = null;
+            solve();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void solve() {
+        readData();
+    }
+
+    private void readData() {
+        this.n = nextInt();
+        this.k = nextInt();
+        this.mxw = nextLong();
+        this.mxp = nextLong();
+
+        this.a = nextLong();
+
+
+        this.b = nextInt();
+
+        this.holder = new HashMap<>();
+
+        for (int i = 0; i < n ; i++) {
+            holder.put(nextLong(), nextLong());
+        }
+    }
+
+    private long nextLong() {
+        return parseLong(nextToken());
+    }
+
+    private int nextInt() {
+        return parseInt(nextToken());
+    }
+
+    private String nextToken() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    private void println(Object o) {
+        print(o);
+        println();
+    }
+
+    private void print(Object o) {
+        writer.print(o);
+    }
+
+    private void flush() {
+        writer.flush();
+    }
+
+    private void println() {
+        writer.println();
+    }
+}
