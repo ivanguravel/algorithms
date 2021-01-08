affd6b6579ac8853cd18fc73568a7ba1734c80bd
ivanguravel <ivanguravel@gmail.com>
2021-01-08 19:21:22 +0200

    Update Mahjong.java

--- src/main/java/org/ivzh/backtracking/Mahjong.java
+++ src/main/java/org/ivzh/backtracking/Mahjong.java
@@ -1,15 +1,16 @@
 package org.ivzh.backtracking;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Mahjong {
 
 
+    private static boolean IS_TENPAI = false;
+
+
     public static void main(String[] args) {
-        String s = "1a 2a 3a 6a 7a 8a 5b 5b 5b 9c 8c 7c 1c 1c";
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
         new Mahjong().solve(s);
     }
 
@@ -18,20 +19,35 @@ public class Mahjong {
         Arrays.sort(split, Comparator.comparingInt((String o) -> o.charAt(1)).thenComparingInt(o -> o.charAt(0)));
         System.out.println(Arrays.toString(split));
         backtracking(Arrays.asList(split), false, 0);
+        if (IS_TENPAI) {
+            System.out.println("Tenpai");
+        } else {
+            System.out.println("Noten");
+        }
     }
 
     private void backtracking(List<String> container, boolean isPairPresent, int tripletCount) {
 
-        if (container.isEmpty() || (isPairPresent && tripletCount == 4)) {
+
+
+        if (container.isEmpty()) {
             System.out.println("Tsumo");
             System.exit(0);
         }
+
+
+        if (((!isPairPresent && tripletCount == 4) || (isPairPresent && tripletCount == 3)) && isTenpaiPresent(container)) {
+            IS_TENPAI = true;
+        }
+
 
         if (!isPairPresent) {
             int prev = 0;
             int i;
             for (i = 1; i < container.size(); i++) {
                 if (container.get(prev).equalsIgnoreCase(container.get(i))) {
+
+
 //                    if ((i < (container.size() - 1) && !container.get(i).equalsIgnoreCase(container.get(i+1)))) {
 //                        i = i+2;
 //                        prev = i+1;
@@ -55,7 +71,6 @@ public class Mahjong {
             int second = 1;
             for (int i = 2; i < container.size(); i++) {
                 if (container.get(i).equalsIgnoreCase(container.get(second)) && container.get(first).equalsIgnoreCase(container.get(second))) {
-                    ++tripletCount;
                     List<String> newContainer = new ArrayList<>();
                     newContainer.addAll(container);
                     newContainer.remove(i);
@@ -87,6 +102,39 @@ public class Mahjong {
             }
         }
 
+    }
+
+    private boolean isTenpaiPresent(List<String> container) {
+        if (container.size() == 2) {
+            return true;
+        }
+
+
+        if (container.get(0).equalsIgnoreCase(container.get(1))
+                || container.get(1).equalsIgnoreCase(container.get(2))
+                || container.get(0).equalsIgnoreCase(container.get(2))) {
+            return true;
+        }
+
+        int firstTriplet = (container.get(1).charAt(0) - container.get(0).charAt(0));
+        int secondTriplet = (container.get(2).charAt(0) - container.get(0).charAt(0));
+        int thirdTriplet = (container.get(2).charAt(0) - container.get(1).charAt(0));
+
+
+        if ( firstTriplet <= 2 && firstTriplet > 0 &&
+                container.get(0).charAt(1) == container.get(1).charAt(1) ||
+
+                secondTriplet <= 2 && secondTriplet > 0 &&
+                        container.get(2).charAt(1) == container.get(0).charAt(1) ||
+
+                thirdTriplet <= 2 && thirdTriplet > 0 &&
+                        container.get(2).charAt(1) == container.get(1).charAt(1)
+
+        ) {
+            return true;
+        }
+
+        return false;
     }
 
     private int findNext(List<String> container, int currentPosition) {
