44be797f59747f61225e92ce532be08550c41452
ivanguravel <ivanguravel@gmail.com>
2021-01-14 12:00:07 +0200

    Create InvertBinaryTree.java

---
+++ src/main/java/org/ivzh/tree/InvertBinaryTree.java
@@ -0,0 +1,31 @@
+package org.ivzh.tree;
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        inventor(root);
+        return root;
+    }
+
+    private void inventor(TreeNode root) {
+        if (root != null) {
+            TreeNode buf = root.left;
+            root.left = root.right;
+            root.right = buf;
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
