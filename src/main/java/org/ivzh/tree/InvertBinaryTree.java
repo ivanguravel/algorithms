35bf851a50b09391ba002230c563e522bafeba64
ivanguravel <ivanguravel@gmail.com>
2021-01-14 12:00:39 +0200

    Update InvertBinaryTree.java

--- src/main/java/org/ivzh/tree/InvertBinaryTree.java
+++ src/main/java/org/ivzh/tree/InvertBinaryTree.java
@@ -1,7 +1,9 @@
 package org.ivzh.tree;
 
+// https://leetcode.com/problems/invert-binary-tree/
 public class InvertBinaryTree {
 
+    
     public TreeNode invertTree(TreeNode root) {
         inventor(root);
         return root;
