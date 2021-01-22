b712eefead0ba423c305e1a38c14afe20cea0597
Ivan Zhuravel <ivanguravel@gmail.com>
2021-01-22 22:10:46 +0200

    fix

--- src/main/java/org/ivzh/tree/AmbitiousExperiment.java
+++ src/main/java/org/ivzh/tree/AmbitiousExperiment.java
@@ -48,9 +48,10 @@ public class AmbitiousExperiment {
         }
 
         for (Integer i : metrics) {
-            p.println(i);
+            p.println(a[i-1]);
+            p.flush();
         }
-        p.flush();
+
     }
 
 
