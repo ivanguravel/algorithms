f26d38d656824f2c0303ed978a440a67c36724ee
Ivan Zhuravel <ivanguravel@gmail.com>
2021-01-23 16:54:28 +0200

    pq

--- src/main/java/org/ivzh/two/pointers/SlidingWindowMaximum.java
+++ src/main/java/org/ivzh/two/pointers/SlidingWindowMaximum.java
@@ -7,8 +7,8 @@ import java.util.*;
 public class SlidingWindowMaximum {
 
     public static void main(String[] args) {
-        int[] n = {4, -2};
-        System.out.println(Arrays.toString(new SlidingWindowMaximum().maxSlidingWindow(n, 2)));
+        int[] n = {1,3,-1,-3,5,3,6,7};
+        System.out.println(Arrays.toString(new SlidingWindowMaximum().maxSlidingWindowPQ(n, 3)));
     }
 
     // O(n) - works for small amounts of data
@@ -20,7 +20,8 @@ public class SlidingWindowMaximum {
             result[0] = n[0];
             return result;
         } else {
-            List<Integer> result = new ArrayList<>();
+            int[] res = new int[n.length - k + 1];
+            int index = 0;
             int first = 0;
             int second = 0;
             int count = 0;
@@ -29,15 +30,45 @@ public class SlidingWindowMaximum {
                 if (count++ < k) {
                     max = Math.max(max, n[second++]);
                 } else {
-                    result.add(max);
+                    res[index++] = max;
                     ++first;
                     second = first;
                     count = 0;
                     max = Integer.MIN_VALUE;
                 }
             }
-            result.add(max);
-            return result.stream().mapToInt(i-> i).toArray();
+            res[index++] = max;
+            return res;
+        }
+    }
+
+    public int[] maxSlidingWindowPQ(int[] n, int k) {
+        if (n.length == 0) {
+            return new int[0];
+        } else if (n.length == 1 && k == 1) {
+            int[] result = new int[1];
+            result[0] = n[0];
+            return result;
+        } else {
+            int[] res = new int[n.length - k + 1];
+            int index = 0;
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
+            int first = 0;
+            int second = 0;
+            int count = 0;
+            while (second < n.length) {
+                if (count++ < k) {
+                    priorityQueue.add(n[second++]);
+                } else {
+                    ++first;
+                    second = first;
+                    count = 0;
+                    res[index++] = Integer.valueOf(priorityQueue.poll()).intValue();
+                    priorityQueue.clear();
+                }
+            }
+            res[index++] = Integer.valueOf(priorityQueue.poll()).intValue();
+            return res;
         }
     }
 }
