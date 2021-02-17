c779e7c6b64c2d3ca58f282d77cbf0ff5df67093
ivanguravel <ivanguravel@gmail.com>
2021-02-17 20:15:16 +0200

    Update MDPARandMIIAR.java

--- src/main/java/org/ivzh/graph/MDPARandMIIAR.java
+++ src/main/java/org/ivzh/graph/MDPARandMIIAR.java
@@ -1,7 +1,7 @@
 package org.ivzh.graph;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 import static java.lang.Integer.parseInt;
 import static java.lang.Long.parseLong;
@@ -9,13 +9,21 @@ import static java.lang.Long.parseLong;
 // https://timus.online/problem.aspx?space=1&num=1315
 public class MDPARandMIIAR {
 
+    private static final char WATTER = 'w';
+    private static final char AIR = '.';
+    private static final char WALL = '#';
+
+
+    private static Pair[] PAIRS = {new Pair(0, 1), new Pair(1, 0), new Pair(0, -1), new Pair(-1, 0)};
+
     int w;
     int h;
     int x;
     int y;
     int d;
     char[][] matrix;
-    boolean[][] isWatterPresent;
+    Pair[][] airAmount;
+    DSU dsu;
 
     private BufferedReader reader;
     private StringTokenizer tokenizer;
@@ -41,15 +49,16 @@ public class MDPARandMIIAR {
     private void solve() {
         this.w = nextInt();
         this.h = nextInt();
-        this.x = nextInt();
-        this.y = nextInt();
+        this.y = nextInt() - 1;
+        this.x = this.h - nextInt() + 1;
         this.d = nextInt();
 
         this.matrix = new char[h+1][w+1];
-        this.isWatterPresent = new boolean[h+1][w+1];
+        this.airAmount = new Pair[h+1][w+1];
+
 
         for (int j =0; j < w; j++) {
-            matrix[0][j] = 'w';
+            matrix[0][j] = WATTER;
         }
 
         for (int i = 1; i<= h; i++) {
@@ -59,15 +68,87 @@ public class MDPARandMIIAR {
             }
         }
 
-        for (int i = 0; i<= h; i++) {
-            for (int j = 0; j <= w; j++) {
-                System.out.print(matrix[i][j]);
+        bfs();
+        provisionAirAmount();
+
+
+            int connectedComponent = dsu.get(airAmount[this.x][this.y].x);
+
+            for (int j =0; j < this.w; j++) {
+                if (airAmount[0][j] != null) {
+                    if (airAmount[0][j].x == connectedComponent) {
+                        println("Can be rescued by himself");
+                        flush();
+                        return;
+                    }
+                }
             }
-            System.out.println();
-        }
+
+
+        println("Rescue operation required");
+        flush();
     }
 
     private void bfs() {
+        Deque<Pair> q = new ArrayDeque<>();
+        for (int j = 0; j < w; j++) {
+            q.add(new Pair(0, j));
+        }
+        Pair p;
+        Pair it;
+        while(!q.isEmpty()) {
+            p = q.peekFirst();
+            q.pollFirst();
+            for (int i = 0; i < PAIRS.length -1; i++) {
+                it = PAIRS[i];
+                int x = p.x + it.x;
+                int y = p.y + it.y;
+                if (x >= 0 && x <= h && y >= 0 && y < w && matrix[x][y] == AIR) {
+                    matrix[x][y] = WATTER;
+                    q.add(new Pair(x, y));
+                }
+            }
+        }
+    }
+
+    private void provisionAirAmount() {
+        int count = 1;
+        Queue<Pair> q = new LinkedList<>();
+        for (int i = 1; i<= h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (matrix[i][j] == AIR) {
+                    airAmount[i][j] = new Pair(count++, 0);
+                    q.add(new Pair(i, j));
+                }
+            }
+        }
+        q.add(new Pair(this.x, this.y));
+        airAmount[this.x][this.y] = new Pair(0, 0);
+        this.dsu = new DSU(count);
+        Pair temp;
+        while (!q.isEmpty()) {
+            temp = q.peek();
+            q.poll();
+            Pair p = airAmount[temp.x][temp.y];
+            if (p.y >= d) {
+                continue;
+            }
+            for (Pair it : PAIRS) {
+                int x = temp.x + it.x;
+                int y = temp.y + it.y;
+                if (x >= 0 && x <= h && y >= 0 && y < w && matrix[x][y] != WALL && airAmount[x][y] == null) {
+                    q.add(new Pair(x, y));
+                    airAmount[x][y] = new Pair(p.x, p.y + 1);
+                }
+                if (x >= 0 && x <= h && y >= 0 && y < w && matrix[x][y] != WALL && airAmount[x][y] != null) {
+                    Pair np = airAmount[x][y];
+                    int currentAirAmount = p.y + np.y + 1;
+                    if (currentAirAmount <= this.d) {
+                        dsu.join(p.x, np.x);
+                    }
+                }
+            }
+        }
 
     }
 
@@ -105,5 +186,61 @@ public class MDPARandMIIAR {
 
     private void println() {
         writer.println();
+    }
+
+    static class Pair {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    static class DSU {
+        int[] parents = new int[500*500+1];
+        int[] dsuSize = new int[500*500+1];
+
+        public DSU(int n) {
+            for (int i = 0; i <= n; i++) {
+                dsuSize[i] = 1;
+                parents[i] = i;
+            }
+        }
+
+        public void join(int a, int b) {
+            int parentA = get(a);
+            int parentB = get(b);
+
+            if (parentA == parentB) {
+                return;
+            }
+
+            if (dsuSize[parentA] > dsuSize[parentB]) {
+                swap(parentA, parentB);
+            }
+
+
+            dsuSize[parentB] = dsuSize[parentB] + dsuSize[parentA];
+
+            parents[parentA] = parentB;
+        }
+
+        public int get(int vertex) {
+            int parent = parents[vertex];
+            if (vertex == parent) {
+                return vertex;
+            } else {
+                return parents[vertex] = get(parent);
+            }
+        }
+
+        private void swap(int a, int b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
     }
 }
