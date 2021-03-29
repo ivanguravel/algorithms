d8a35af252725628e39d090215f1bdccdf97ed7e
Ivan Zhuravel <ivanguravel@gmail.com>
2021-03-29 22:08:18 +0300

    https://algocademy.com/app/#problem/longest-subarray-without-reapeating

--- src/main/java/org/ivzh/sort/QuickSort.java
+++ src/main/java/org/ivzh/sort/QuickSort.java
@@ -6,5 +6,36 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class QuickSort {
 
+    public static void main(String[] args) {
+        int []a = {2, 1, 6, 5, 3, 4};
+        qSort(a, 0, a.length);
+        System.out.println(Arrays.toString(a));
+    }
 
+    private static void qSort(int[] a, int l, int r) {
+        if (r - l <= 1) {
+            return;
+        } else {
+            int m = l;
+            int pivot = a[random(l, r-1)];
+            for (int i = l; i < r; i++) {
+                if (a[i] < pivot) {
+                    swap(a, i, m);
+                    m++;
+                }
+            }
+            qSort(a, l, m);
+            qSort(a, m, r);
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int k = a[i];
+        a[i] = a[j];
+        a[j] = k;
+    }
+
+    public static int random(int min,int max) {
+        return min + (int)(Math.random() * ((max - min) + 1));
+    }
 }
