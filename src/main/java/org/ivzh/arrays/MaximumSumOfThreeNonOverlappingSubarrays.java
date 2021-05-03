b4f06165f0a8edd812af43fae81bcac2ba3c3250
ivanguravel <ivanguravel@gmail.com>
2021-05-03 09:39:27 +0300

    Update MaximumSumOfThreeNonOverlappingSubarrays.java

--- src/main/java/org/ivzh/arrays/MaximumSumOfThreeNonOverlappingSubarrays.java
+++ src/main/java/org/ivzh/arrays/MaximumSumOfThreeNonOverlappingSubarrays.java
@@ -3,6 +3,8 @@ package org.ivzh.arrays;
 
 // https://algocademy.com/app/#problem/max-sum-of-three-subarrays
 public class MaximumSumOfThreeNonOverlappingSubarrays {
+    
+    // O(n^6)
     public int maxSumOfThreeSubarrays(int[] nums) {
         int n = nums.length;
         int maxSum = Integer.MIN_VALUE;
@@ -26,5 +28,32 @@ public class MaximumSumOfThreeNonOverlappingSubarrays {
             }
         }
         return maxSum;
+    }
+    
+    // O(n^3)
+    public int maxSumOfThreeSubarrays(int[] nums) {
+        int n = nums.length;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
+            int sum1 = this.maxSumSubarray(Arrays.copyOfRange(nums, 0, i));
+            int sum2 =0;
+            for (int j = i; j < n - 1; j++) {
+                sum2 = sum2 + nums[j];
+                int sum3 = this.maxSumSubarray(Arrays.copyOfRange(nums, j+1, n));
+                maxSum = Math.max(maxSum, sum3 + sum2 + sum1);
+            }
+        }
+        return maxSum;
+    }
+    
+    private int maxSumSubarray(int[] nums) {
+        int result = nums[0];
+        int sum = 0;
+        for (int i : nums) {
+            sum = i + Math.max(0, sum);
+            result = Math.max(result, sum);
+        }
+        
+        return result;
     }
 }
