113c228828aff1be093c1c480678a5cbe07da4a9
Ivan Zhuravel <ivanguravel@gmail.com>
2021-09-18 16:58:26 +0300

    https://leetcode.com/problems/permutations/

---
+++ src/main/java/org/ivzh/algebra/HeapAlgorithm.java
@@ -0,0 +1,52 @@
+package org.ivzh.algebra;
+
+
+import java.util.*;
+
+// https://leetcode.com/problems/permutations/
+public class HeapAlgorithm {
+
+    public static void main(String[] args) {
+
+        int[] n = {1, 2, 3};
+
+        HeapAlgorithm heapAlgorithm = new HeapAlgorithm();
+        List<List<Integer>> results = heapAlgorithm.permute(n);
+//        for (List<Integer> l : results) {
+//            System.out.println(l);
+//        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        generatePermutationsHeap(results, nums, nums.length);
+        return results;
+    }
+
+
+    private void generatePermutationsHeap(List<List<Integer>> results, int[] nums, int k) {
+        if (k == 1) {
+            List<Integer> l = new LinkedList<>();
+            for (int i : nums) {
+                l.add(i);
+            }
+            results.add(l);
+            return;
+        }
+        for (int i = 0; i < k; i++) {
+            generatePermutationsHeap(results, nums, k-1);
+            if (k % 2 == 0) {
+               swap(nums, i, k-1);
+            } else {
+                swap(nums, 0, k-1);
+            }
+        }
+
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
