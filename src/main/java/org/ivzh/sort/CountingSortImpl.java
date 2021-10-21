3de9eb86045bc4c0fc396f79d61b65a70de07ec0
ivanguravel <ivanguravel@gmail.com>
2021-10-21 23:18:16 +0300

    Create CountingSortImpl.java

---
+++ src/main/java/org/ivzh/sort/CountingSortImpl.java
@@ -0,0 +1,29 @@
+package org.ivzh.sort;
+
+
+public class CountingSortImpl {
+
+    public static void countingSort(int[] a){
+        
+        int[] counter = new int[10];
+        
+        int[] result = new int[a.length];
+        
+        for (int i : a) {
+            counter[i] += 1;
+        }
+        
+        for (int i = 1; i < counter.length; i++) {
+            counter[i] = counter[i] + counter[i-1];
+        }
+        
+        for (int i = a.length -1; i >= 0; i--) {
+            int index = counter[a[i]] - 1;
+            counter[a[i]] = counter[a[i]] - 1;
+            result[index] = a[i];
+        }
+        
+        System.out.println(java.util.Arrays.toString(result));
+     }
+
+}
