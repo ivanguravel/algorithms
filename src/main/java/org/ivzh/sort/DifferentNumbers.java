372bf340e98532490a46dceb5b7e7faf5467334c
Ivan Zhuravel <ivanguravel@gmail.com>
2021-11-14 14:29:38 +0200

    https://informatics.msk.ru/mod/statements/view.php?chapterid=1418#1

---
+++ src/main/java/org/ivzh/sort/DifferentNumbers.java
@@ -0,0 +1,144 @@
+package org.ivzh.sort;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+// https://informatics.msk.ru/mod/statements/view.php?chapterid=1418#1
+public class DifferentNumbers {
+    int[] arr;
+    long different;
+
+    private void solve() {
+        int n = nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i-1] != arr[i]) {
+                ++different;
+            }
+        }
+
+        println(different+1);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+
+
+    public static void main(String[] args) {
+        new DifferentNumbers().run();
+    }
+
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+    private PrintWriter writer;
+
+    private void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out))) {
+            this.reader = reader;
+            this.writer = writer;
+            this.tokenizer = null;
+            solve();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private int nextInt(int radix) {
+        return parseInt(nextToken(), radix);
+    }
+
+    private int nextInt() {
+        return parseInt(nextToken());
+    }
+
+    private long nextLong(int radix) {
+        return parseLong(nextToken(), radix);
+    }
+
+    private long nextLong() {
+        return parseLong(nextToken());
+    }
+
+    private double nextDouble() {
+        return parseDouble(nextToken());
+    }
+
+    private int[] nextArr(int size) {
+        return stream(new int[size]).map(c -> nextInt()).toArray();
+    }
+
+    private long[] nextArrL(int size) {
+        return stream(new long[size]).map(c -> nextLong()).toArray();
+    }
+
+    private double[] nextArrD(int size) {
+        return stream(new double[size]).map(c -> nextDouble()).toArray();
+    }
+
+    private char[][] nextCharMatrix(int n) {
+        return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
+    }
+
+    private int[][] nextIntMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(int[][]::new);
+    }
+
+    private double[][] nextDoubleMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArrD(m)).toArray(double[][]::new);
+    }
+
+    private String nextToken() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    private void printf(String format, Object... args) {
+        writer.printf(format, args);
+    }
+
+    private void print(Object o) {
+        writer.print(o);
+    }
+
+    private void println() {
+        writer.println();
+    }
+
+    private void println(Object o) {
+        print(o);
+        println();
+    }
+
+    private void flush() {
+        writer.flush();
+    }
+
+
+
+}
+
