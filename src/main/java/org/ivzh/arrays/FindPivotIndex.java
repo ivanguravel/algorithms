31f5c78e3f03f840fdc96f5b4b7bf44077a05c4e
ivanguravel <ivanguravel@gmail.com>
2022-09-24 16:42:51 +0300

    Create FindPivotIndex.java

---
+++ src/main/java/org/ivzh/arrays/FindPivotIndex.java
@@ -0,0 +1,62 @@
+package org.ivzh.arrays; 
+
+
+import java.util.*;
+
+// https://leetcode.com/problems/find-pivot-index/
+public class FindPivotIndex {
+
+  public int pivotIndex(int[] nums) {
+        int[] sufix = new int[nums.length];
+        sufix[nums.length-1] = nums[nums.length-1];
+        
+        for (int i =nums.length-2; i >= 0; i--) {
+            sufix[i] = sufix[i+1] + nums[i];
+        }
+        
+        
+        int current = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int currentSuffix = sufix[i] - nums[i];
+                
+            if (currentSuffix == current) {
+                return i;
+            }
+            
+            current += nums[i];
+        }
+        
+        return -1;
+  }
+	
+	
+	public int pivotIndex(int[] nums) {
+        int[] sufix = new int[nums.length];
+        sufix[nums.length-1] = nums[nums.length-1];
+        
+        for (int i =nums.length-2; i >= 0; i--) {
+            sufix[i] = sufix[i+1] + nums[i];
+        }
+        
+        int[] prefix  = new int[nums.length];
+        prefix[0] = nums[0];
+        
+         for (int i =1; i < nums.length; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
+         }
+        
+        
+        for (int i = 0; i < nums.length; i++) {
+            int currentSufix = sufix[i] - nums[i];
+            int currentPrefix = prefix[i] - nums[i];
+                
+            if (currentPrefix == currentSufix) {
+                return i;
+            }
+            
+        }
+        
+        return -1;
+    }
+
+}
