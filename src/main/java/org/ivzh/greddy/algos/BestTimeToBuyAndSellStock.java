2ee2906aa50c925f8d6e6ad28ee7478a0e7eb7a0
ivanguravel <ivanguravel@gmail.com>
2022-11-19 23:05:48 +0200

    Update BestTimeToBuyAndSellStock.java

--- src/main/java/org/ivzh/greddy/algos/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/ivzh/greddy/algos/BestTimeToBuyAndSellStock.java
@@ -6,6 +6,7 @@ import java.util.*;
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 public class BestTimeToBuyAndSellStock {
     
+    // kadane
      public int maxProfit(int[] prices) {
         int price = Integer.MAX_VALUE;
         int profit = 0;
@@ -15,7 +16,24 @@ public class BestTimeToBuyAndSellStock {
             else if (prices[i] - price > profit)
                 profit = prices[i] - price;
         }
+        if (profit < 0) {
+            profit = 0;
+        }
         return profit;
+    }
+    
+    public int maxProfitTrivial(int[] prices) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i+1; j < prices.length; j++) {
+                max = Math.max(prices[j] - prices[i], max);
+                
+            }
+        }
+        if (max < 0) {
+            max = 0;
+        }
+        return max;
     }
 
 }
