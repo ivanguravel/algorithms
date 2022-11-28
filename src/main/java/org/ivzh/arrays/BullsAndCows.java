099f01f7ab49b9a32c78e27906b724d1c1ca7d17
ivanguravel <ivanguravel@gmail.com>
2022-11-28 12:56:37 +0200

    Create BullsAndCows.java

---
+++ src/main/java/org/ivzh/arrays/BullsAndCows.java
@@ -0,0 +1,36 @@
+package org.ivzh.arrays;
+
+import java.util.*;
+
+// https://leetcode.com/problems/bulls-and-cows/
+public class BullsAndCows {
+  
+  public String getHint(String secret, String guess) {
+        int[] h = new int[10];
+            
+        int bulls = 0, cows = 0;
+        int n = guess.length();
+        for (int idx = 0; idx < n; ++idx) {
+            char s = secret.charAt(idx);
+            char g = guess.charAt(idx);
+            if (s == g) {
+                bulls++;    
+            } else {
+                if (h[s - '0'] < 0) 
+                    cows++;
+                if (h[g - '0'] > 0)
+                    cows++;
+                h[s - '0']++;
+                h[g - '0']--;
+            }
+        } 
+                
+        StringBuilder sb = new StringBuilder();
+        sb.append(bulls); 
+        sb.append("A"); 
+        sb.append(cows); 
+        sb.append("B");
+        return sb.toString();
+    }
+  
+}
