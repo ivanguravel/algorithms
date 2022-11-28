1b87d4c45b4de1e1931239f52dc6b86e122b51e1
ivanguravel <ivanguravel@gmail.com>
2022-11-28 13:04:16 +0200

    Create DecodeString.java

---
+++ src/main/java/org/ivzh/strings/DecodeString.java
@@ -0,0 +1,37 @@
+package org.ivzh.strings;
+
+import java.util.*;
+
+// https://leetcode.com/problems/decode-string/
+public class DecodeString {
+  
+  public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + ch - '0';
+            } else if (ch == '[') {
+                // push the number k to countStack
+                countStack.push(k);
+                // push the currentString to stringStack
+                stringStack.push(currentString);
+                // reset currentString and k
+                currentString = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder decodedString = stringStack.pop();
+                // decode currentK[currentString] by appending currentString k times
+                for (int currentK = countStack.pop(); currentK > 0; currentK--) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
+        }
+        return currentString.toString();
+    }
+}
