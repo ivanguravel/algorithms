0245e1f56424cadcce2f15e31c05be1fa55f8a29
ivanguravel <ivanguravel@gmail.com>
2022-12-13 18:25:49 +0200

    Create BusRoutes.java

---
+++ src/main/java/org/ivzh/graph/BusRoutes.java
@@ -0,0 +1,46 @@
+package org.ivzh.graph;
+
+import java.io.*;
+import java.util.*;
+
+
+// https://leetcode.com/problems/bus-routes
+public class BusRoutes {
+
+    public int numBusesToDestination(int[][] routes, int S, int T) {
+       HashSet<Integer> visited = new HashSet<>();
+       Queue<Integer> q = new LinkedList<>();
+       HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+       int ret = 0; 
+        
+       if (S==T) return 0; 
+        
+       for(int i = 0; i < routes.length; i++){
+            for(int j = 0; j < routes[i].length; j++){
+                ArrayList<Integer> buses = map.getOrDefault(routes[i][j], new ArrayList<>());
+                buses.add(i);
+                map.put(routes[i][j], buses);                
+            }       
+        }
+                
+       q.offer(S); 
+       while (!q.isEmpty()) {
+           int len = q.size();
+           ret++;
+           for (int i = 0; i < len; i++) {
+               int cur = q.poll();
+               ArrayList<Integer> buses = map.get(cur);
+               for (int bus: buses) {
+                    if (visited.contains(bus)) continue;
+                    visited.add(bus);
+                    for (int j = 0; j < routes[bus].length; j++) {
+                        if (routes[bus][j] == T) return ret;
+                        q.offer(routes[bus][j]);  
+                   }
+               }
+           }
+        }
+        return -1;
+    }
+
+}
