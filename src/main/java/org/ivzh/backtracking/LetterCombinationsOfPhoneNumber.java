aad24e628d7fd6f60a94ea344092e9ebfa40e559
izhuravel <ivanguravel@gmail.com>
2023-02-12 21:10:15 +0200

    letter combinations of the phone number

--- src/main/java/org/ivzh/backtracking/LetterCombinationsOfPhoneNumber.java
+++ src/main/java/org/ivzh/backtracking/LetterCombinationsOfPhoneNumber.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class LetterCombinationsOfPhoneNumber {
 
     private Map<Character, String> keys = new HashMap<>();
-    private Set<String> set = new HashSet<>();
 
     public LetterCombinationsOfPhoneNumber() {
         keys.put('2', "abc");
@@ -19,16 +18,31 @@ public class LetterCombinationsOfPhoneNumber {
     }
 
 
-    public static void main(String[] args) {
-        new LetterCombinationsOfPhoneNumber().letterCombinations("23");
-    }
-
     public List<String> letterCombinations(String digits) {
-        return new ArrayList<>(set);
+        if ("".equals(digits)) {
+            return Collections.emptyList();
+        }
+
+        List<String> result = new ArrayList<>();
+        backtracking(digits, 0, result, digits.length(), new LinkedList<>());
+        return result;
     }
 
-    private void backtracking(String digits, int position) {
-        char c = digits.charAt(position);
+    private void backtracking(String digits, int position, List<String> result, int n, LinkedList<String> buff) {
+        if (buff.size() == n) {
+            String res = "";
+            for (String s : new HashSet<>(buff)) {
+                res = res + s;
+            }
+            result.add(res);
+            return;
+        }
 
+        String letters = keys.get(digits.charAt(position));
+        for (int i = 0; i < letters.length(); i++) {
+            buff.add("" + letters.charAt(i));
+            backtracking(digits, position + 1, result, n, buff);
+            buff.removeLast();
+        }
     }
 }
