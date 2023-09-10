68d7269a6fc173de2d0dafff951429b93155cdb9
izhuravel <ivanguravel@gmail.com>
2023-09-10 16:42:13 +0300

    lcs

--- src/main/java/org/ivzh/dynamic/programming/two/dimensions/LongestCommonSubsequence.java
+++ src/main/java/org/ivzh/dynamic/programming/two/dimensions/LongestCommonSubsequence.java
@@ -1,5 +1,7 @@
 package org.ivzh.dynamic.programming.two.dimensions;
 
+import java.util.Arrays;
+
 // https://leetcode.com/problems/longest-common-subsequence/
 public class LongestCommonSubsequence {
 
@@ -51,5 +53,31 @@ public class LongestCommonSubsequence {
                     longestCommonSubsequenceRecursive(text1, text2.substring(0, size2)));
         }
 
+    }
+
+    // recursion with cache
+    public int longestCommonSubsequence3(String text1, String text2) {
+        Integer [][] dp = new Integer[text1.length()][text2.length()];
+        for (Integer[] arr: dp) {
+            Arrays.fill(arr, -100);
+        }
+        return findLCS(text1, text2, text1.length() -1, text2.length() -1, dp);
+    }
+
+    int findLCS(String text1, String text2, int i, int j, Integer [][] dp) {
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+
+        if (dp[i][j] != -100) {
+            return dp[i][j];
+        }
+
+        if (text1.charAt(i) == text2.charAt(j)) {
+            return dp[i][j] = findLCS(text1, text2, i -1, j -1, dp) + 1;
+        }
+
+        dp[i][j] = Math.max(findLCS(text1, text2, i -1, j, dp), findLCS(text1, text2, i, j -1, dp));
+        return dp[i][j];
     }
 }
