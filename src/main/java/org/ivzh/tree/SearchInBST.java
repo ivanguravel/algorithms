d37eb098a0553d9043bebfae5c0ddabc04ac6232
izhuravel <ivanguravel@gmail.com>
2023-10-25 10:50:20 +0300

    https://leetcode.com/problems/search-in-a-binary-search-tree/description/

---
+++ src/main/java/org/ivzh/tree/SearchInBST.java
@@ -0,0 +1,34 @@
+package org.ivzh.tree;
+
+
+// https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+public class SearchInBST {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
