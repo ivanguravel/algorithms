e7557ab00f108384ec8a78f95b288e6313b7465a
ivanguravel <ivanguravel@gmail.com>
2025-08-05 10:42:13 +0200

    Update LongestRepeatingCharacterReplacement.java

--- src/main/java/org/ivzh/two/pointers/LongestRepeatingCharacterReplacement.java
+++ src/main/java/org/ivzh/two/pointers/LongestRepeatingCharacterReplacement.java
@@ -5,7 +5,14 @@ import java.util.Set;
 
 // https://leetcode.com/problems/longest-repeating-character-replacement/
 public class LongestSubstringWithoutRepeatingCharacters {
-    
+
+
+    // Используем скользящее окно (sliding window).
+    // Но — вместо того чтобы фиксировать какой символ мы хотим получить, 
+    // мы просто отслеживаем частоту самого частого символа в окне.
+    // Если длина текущего окна right - left + 1
+    // и мы знаем, что максимальная частота символа в окне = maxFreq,
+    // то мы должны заменить windowLength - maxFreq символов, чтобы всё стало одинаковым. Если windowLength - maxFreq <= k → это валидное окно
     public int characterReplacement(String s, int k) {
         int[] freq = new int[26];
         int left = 0;
