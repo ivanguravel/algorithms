de72668584e1b5187efba0f2eb21b075511f956f
ivanguravel <ivanguravel@gmail.com>
2025-08-31 15:11:22 +0200

    Update LRUCache.java

--- src/main/java/org/ivzh/cache/LRUCache.java
+++ src/main/java/org/ivzh/cache/LRUCache.java
@@ -8,27 +8,75 @@ import java.util.Map;
 // https://leetcode.com/problems/lru-cache/
 public class LRUCache {
 
-    int capacity;
-    LinkedHashMap<Integer, Integer> hash;
+ private int capacity;
+    private Map<Integer, Node> cache;
+    private Node oldest;
+    private Node latest;
+
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.hash = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
-            protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest) {
-                return size() > capacity;
-            }   
-        };
+        this.cache = new HashMap<>();
+        this.oldest = new Node(0, 0);
+        this.latest = new Node(0, 0);
+        this.oldest.next = this.latest;
+        this.latest.prev = this.oldest;
     }
+
+
     
     public int get(int key) {
-        Integer val = hash.get(key);
-        return val == null ? -1 : val;
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            remove(node);
+            insert(node);
+            return node.val;
+        }
+        return -1;
     }
     
     public void put(int key, int value) {
-        hash.put(key, value);
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+        Node newNode = new Node(key, value);
+        cache.put(key, newNode);
+        insert(newNode);
+
+        if (cache.size() > capacity) {
+            Node lru = oldest.next;
+            remove(lru);
+            cache.remove(lru.key);
+        }
     }
 
+    private void remove(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
 
+    private void insert(Node node) {
+        Node prev = latest.prev;
+        Node next = latest;
+        prev.next = next.prev = node;
+        node.next = next;
+        node.prev = prev;
+    }
+
+    static class Node {
+        int key;
+        int val;
+        Node prev;
+        Node next;
+
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.prev = null;
+            this.next = null;
+        }
+   }
 
 }
